5923646eb8392781633a25855877e854dfeb839a
Elijah Ewers <ewerselijah@gmail.com>
2025-02-06 11:40:05 -0500

    Finished implementation of gradient container and centered widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,13 @@
 import 'package:flutter/material.dart';
+import 'gradient_container.dart';
+
 
 void main(){
     runApp(
       MaterialApp(
           home: Scaffold(
-            body: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin:Alignment.topCenter,
-                
-                  colors: <Color>[
-                    Color.fromARGB(243, 255, 55, 24),
-                    Color.fromARGB(243, 24, 74, 255),
-                    Color.fromARGB(243, 255, 24, 182),
-                  ]
-                )
-              ),
-              child: const Center(
-                child:Text("Hello World",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 180, 7, 7),
-                  fontSize: 35,
-                )),
-                ),
-            ),
-              backgroundColor: Color.fromARGB(243, 24, 74, 255)
-          )
+            body: GradientContainer(),
+          ),
       ),
     );
 }
